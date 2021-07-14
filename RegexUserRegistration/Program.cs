4aebec4183629f8c5cdbe738d6310094e587f2cd
AkshayKumarTN <tnak369@gmail.com>
2021-07-14 20:25:10 +0530

    [Akshay] Added ->LastName Validation method

--- RegexUserRegistration/Program.cs
+++ RegexUserRegistration/Program.cs
@@ -7,13 +7,22 @@ namespace RegexUserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("\n Regex User Registration Validation");
+
             RegexUserValidation regexUser = new RegexUserValidation();
+
             Console.Write("\n Enter First Name : ");
             string firstName = Convert.ToString(Console.ReadLine());
+            Console.Write(" Enter Last Name : ");
+            string lastName = Convert.ToString(Console.ReadLine());
+
             if (regexUser.ValidateFirstName(firstName))
-                Console.WriteLine(" First Name \"" + firstName + "\" is Valid");
+                Console.WriteLine("\n First Name \"" + firstName + "\" is Valid");
             else
                 Console.WriteLine(" First Name \"" + firstName + "\" is InValid");
+            if (regexUser.ValidateLastName(lastName))
+                Console.WriteLine(" Last Name \"" + lastName + "\" is Valid");
+            else
+                Console.WriteLine(" Last Name \"" + lastName + "\" is InValid");
 
 
         }
--- RegexUserRegistration/RegexUserValidation.cs
+++ RegexUserRegistration/RegexUserValidation.cs
@@ -8,10 +8,15 @@ namespace RegexUserRegistration
     class RegexUserValidation
     {
         public const string UserFirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public const string UserLastName = "^[A-Z]{1}[A-Za-z]{2,}$";
 
         public bool ValidateFirstName(String firstName)
         {
             return Regex.IsMatch(firstName, UserFirstName);
+        }
+        public bool ValidateLastName(String lastName)
+        {
+            return Regex.IsMatch(lastName, UserLastName);
         }
 
 
